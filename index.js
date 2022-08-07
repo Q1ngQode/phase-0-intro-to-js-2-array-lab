85c4f9b4fbf420bf615339b1ddbe6b05798e602a
Briana McCloud <bmccloud26122@gmail.com>
2022-08-07 16:59:42 -0400

    commit tree 03dbc108ce3025c944de7dbee976fcc56e145f9f

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+  console.log(name);
+
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name)
+console.log(name);
+
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name)
+console.log(name);
+
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name)
+console.log(name);
+
+}
+
+function appendCat(name) {
+  var name_1 = [...cats, name];
+  return name_1;
+}
+
+function prependCat(name) {
+  var name_2 = [name,... cats];
+  return name_2;
+
+}
+
+function removeLastCat() {
+  var newCats = cats.slice(0,cats.length-1);
+  return newCats;
+}
+
+function removeFirstCat() {
+  var newCats = cats.slice(1);
+  return newCats;
+}
